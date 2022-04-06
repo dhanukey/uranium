c28a30c4678bdae5038ed932f864ed55326f1f42
unknown <you@example.com>
2022-04-06 19:50:25 +0530

    change

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const a = require('../logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    console.log(a.u)
+
 });
 
 module.exports = router;
